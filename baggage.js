ba4e5e16282d25900abfb9f102d34ece8746d9fc
ReesesThesis <salcido.jason@gmail.com>
2020-09-29 17:31:14 -0700

    Add files via upload

---
+++ baggage.js
@@ -0,0 +1,44 @@
+import { onTruck, expandTruck, score, round, TRUCK_SPEED } from './truck.js'
+import { randomGridPosition } from './grid.js'
+
+let baggage = getRandomBaggagePosition()
+const EXPANSION_RATE = 1
+let notStarted = true
+
+export function update() {
+    if (onTruck(baggage)) {
+        expandTruck(EXPANSION_RATE)
+        baggage = getRandomBaggagePosition()
+    }
+
+}
+
+
+export function render(gameBoard) {
+
+    const baggageElement = document.createElement('div')
+    baggageElement.style.gridRowStart = baggage.y
+    baggageElement.style.gridColumnStart = baggage.x
+    baggageElement.classList.add('baggage')
+    gameBoard.appendChild(baggageElement)
+}
+
+function getRandomBaggagePosition() {
+    let newBaggagePosition
+    let number = parseInt(score.innerHTML)
+    while (newBaggagePosition == null || onTruck(newBaggagePosition)) {
+
+        newBaggagePosition = randomGridPosition()
+
+
+    }
+
+    if (parseInt(score.innerHTML) == 0) {
+        return newBaggagePosition
+    }
+
+    number += (15 * round)
+    score.innerHTML = number.toString()
+
+    return newBaggagePosition
+}
---
+++ truck.js
@@ -0,0 +1,111 @@
+import { getInputDirection } from "./input.js"
+
+const countDown = document.querySelector('#timer-output')
+export const score = document.querySelector('#score-output')
+
+export var TRUCK_SPEED = 5
+let gameStarted = false;
+let threeMinutes = 10
+export let round = 1
+
+const truckBody = [
+    { x: 11, y: 11 }
+]
+let newSegments = 0
+
+export function update() {
+    addSegments()
+    const inputDirection = getInputDirection()
+    for (let i = truckBody.length - 2; i >= 0; i--) {
+        truckBody[i + 1] = {...truckBody[i] }
+    }
+
+    truckBody[0].x += inputDirection.x
+    truckBody[0].y += inputDirection.y
+}
+
+export function render(gameBoard) {
+    let count = 1;
+    truckBody.forEach(segment => {
+        if (count === 1) {
+            const truckElement = document.createElement('div')
+            truckElement.style.gridRowStart = segment.y
+            truckElement.style.gridColumnStart = segment.x
+            truckElement.classList.add('truck')
+            gameBoard.appendChild(truckElement)
+            count++;
+        } else {
+            if (gameStarted === false) {
+                startTimer(threeMinutes, countDown)
+                gameStarted = true
+            }
+            const truckElement = document.createElement('div')
+            truckElement.style.gridRowStart = segment.y
+            truckElement.style.gridColumnStart = segment.x
+            truckElement.classList.add('car')
+            gameBoard.appendChild(truckElement)
+        }
+    })
+}
+
+export function expandTruck(amount) {
+    newSegments += amount
+}
+
+export function onTruck(position, { ignoreHead = false } = {}) {
+    return truckBody.some((segment, index) => {
+        if (ignoreHead && index === 0) return false
+        return equalPositions(segment, position)
+    })
+}
+
+export function getTruckHead() {
+    return truckBody[0]
+}
+
+export function truckCollision() {
+    return onTruck(truckBody[0], { ignoreHead: true })
+}
+
+function equalPositions(pos1, pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y
+}
+
+function addSegments() {
+    for (let i = 0; i < newSegments; i++) {
+        truckBody.push({...truckBody[truckBody.length - 1] })
+    }
+
+    newSegments = 0
+}
+
+function startTimer(duration, display) {
+    let num = 0
+    var timer = duration,
+        minutes, seconds;
+    setInterval(function() {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        num = parseInt(score.innerHTML)
+        num += 1
+        score.innerHTML = num.toString()
+        if (--timer <= -1) {
+            round++
+            timer = duration
+            TRUCK_SPEED++;
+            if (round == 30) {
+                if (confirm("You Won!")) {
+                    location.reload()
+                } else {
+                    window.location = '/'
+                }
+            }
+        }
+    }, 1000);
+}
